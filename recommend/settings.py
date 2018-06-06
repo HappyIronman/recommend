330b0e97f2c6b26b433b541429e237e28e53ba3d
HappyIronman <1014566133@qq.com>
2018-06-06 19:53:44 +0800

    created

--- recommend/settings.py
+++ recommend/settings.py
@@ -154,7 +154,10 @@ LOGGING = {
         },
     },
 }
-CRONJOBS = [('*/5 * * * *', 'homepage.task.craw_news_task.offline_craw'),],
+
+CRONJOBS = [('*/1 * * * *', 'homepage.task.craw_news_task.offline_craw', '>> /log/cron.log'),
+            ('*/2 * * * *', 'homepage.engine.engine.offline_cal_recommend_list', '>> /log/cron.log'),
+            ('*/3 * * * *', 'homepage.task.craw_news_task.delete_news', '>> /log/cron.log')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
